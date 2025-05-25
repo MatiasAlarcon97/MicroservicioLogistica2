76cfc0af53c9e6d25858f19b99377fe983ac0777
matiasalarcon6 <mati.gb.6@gmail.com>
2025-05-25 17:47:25 -0400

    Envio

--- src/main/java/com/duoc/MicroservicioLogistica/Model/Envio.java
+++ src/main/java/com/duoc/MicroservicioLogistica/Model/Envio.java
@@ -1,4 +1,17 @@
 package com.duoc.MicroservicioLogistica.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Envio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fecha_envio;
 }
