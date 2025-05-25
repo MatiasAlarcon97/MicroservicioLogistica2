1d2c54524a0e77d9a678d9620cc504c2b817db54
matiasalarcon6 <mati.gb.6@gmail.com>
2025-05-25 18:02:27 -0400

    tercero commit

--- src/main/java/com/duoc/MicroservicioLogistica/Model/Proveedor.java
+++ src/main/java/com/duoc/MicroservicioLogistica/Model/Proveedor.java
@@ -1,4 +1,19 @@
 package com.duoc.MicroservicioLogistica.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String direccion;
+
+    private String contacto;
 }
